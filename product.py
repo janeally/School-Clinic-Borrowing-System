c0d118a337df014b7314202c4dccc9dd8f58857b
Jane Rio Dela Torre <jane.delatorre@hercorcollege.edu.ph>
2026-03-04 21:04:01 +0800

    Cleaned project structure

--- product.py
+++
@@ -1,6 +0,0 @@
-class Product:
-    def __init__(self, product_id, name, stock):
-        self.product_id = product_id
-        self.name = name
-        self.stock = stock
-        self.status = "In Stock" if stock > 0 else "Out of Stock"
--- validator.py
+++
@@ -1,8 +0,0 @@
-class StockValidator:
-    @staticmethod
-    def validate_sale(current_stock, requested_amount):
-        if requested_amount <= 0:
-            return False, "Invalid quantity"
-        if requested_amount > current_stock:
-            return False, "Insufficient stock"
-        return True, "Valid"
